22006f0a4122426c6be82ebefc6ddaf356478716
katapultcloud <40276838+katapultcloud@users.noreply.github.com>
2018-07-20 19:02:48 +0200

    updating licence header and removing unused module

--- price_of_ebs.py
+++ price_of_ebs.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2018, Stefan Roman <stefan.roman@katapult.cloud>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 # import modules
 import boto3
-import pprint
 import sys
 import argparse
 import json
